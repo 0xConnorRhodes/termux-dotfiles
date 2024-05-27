f2fc49a39afa7a12dd4338330a4a2a3b3e4e350e
Connor Rhodes <connor@rhodes.contact>
2024-05-27 12:42:48 -0500

    updated prompt to have path from ~ and colored characters

--- private_dot_config/private_fish/private_functions/private_fish_prompt.fish
+++ private_dot_config/private_fish/private_functions/private_fish_prompt.fish
@@ -1,6 +1,7 @@
 function fish_prompt
     set_color $fish_color_cwd
-    echo -n (basename $PWD)
-    set_color normal
+    echo -n (string replace -r "^$HOME" "~" $PWD)
+    set_color white
+    echo ':'
     echo -n ' > '
 end
