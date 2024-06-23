f6c4429cf83fc5bb273e7aa370bc01aee435884c
Connor Rhodes <connor@rhodes.contact>
2024-06-22 20:28:09 -0500

    added ntk aliases

--- private_dot_config/private_fish/private_aliases.fish
+++ private_dot_config/private_fish/private_aliases.fish
@@ -3,6 +3,7 @@ alias cm="chezmoi"
 alias gs="git status"
 alias cmd="SHELL=/data/data/com.termux/files/usr/bin/fish chezmoi cd"
 alias tk="lua $HOME/code/notes-scripts/tasks.lua"
+alias ntk="lua $HOME/code/notes-scripts/new-task.lua"
 
 # navigation
 alias x="exit"
