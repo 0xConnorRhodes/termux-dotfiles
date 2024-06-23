6bf0e4533a3c7064c44321335549db19218d5915
Connor Rhodes <connor@rhodes.contact>
2024-06-22 20:14:19 -0500

    added tk alias

--- private_dot_config/private_fish/private_aliases.fish
+++ private_dot_config/private_fish/private_aliases.fish
@@ -2,6 +2,7 @@ alias e="nvim"
 alias cm="chezmoi"
 alias gs="git status"
 alias cmd="SHELL=/data/data/com.termux/files/usr/bin/fish chezmoi cd"
+alias tk="lua $HOME/code/notes-scripts/tasks.lua"
 
 # navigation
 alias x="exit"
