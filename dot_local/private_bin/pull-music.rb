02b5278eaa92a1a28f58452da8561dfba89bfa8f
Connor Rhodes <connor@rhodes.contact>
2024-10-31 09:23:50 -0500

    added pull-music.rb

---
+++ dot_local/private_bin/pull-music.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+require 'open3'
+
+remote_path = 's:/zstore/media/music/music_library/music_library/'
+local_folder = File.expand_path('~/music/')
+
+rsync_command = "rsync -rhP '#{remote_path}' '#{local_folder}'"
+
+# Run the rsync command and display live output
+Open3.popen3(rsync_command) do |stdin, stdout, stderr, wait_thr|
+  Thread.new do
+    stdout.each { |line| puts line }
+  end
+  Thread.new do
+    stderr.each { |line| warn line }
+  end
+  wait_thr.value # Wait for the process to finish
+end
