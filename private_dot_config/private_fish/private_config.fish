973598c2b47c369a81a160eca3cbd46c86eaa6c2
Connor Rhodes <connor@rhodes.contact>
2025-06-30 21:28:17 -0500

    prune old dotfiles

--- private_dot_config/private_fish/private_config.fish
+++
@@ -1,9 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    source $HOME/.config/fish/aliases.fish
-end
-
-source $HOME/.config/fish/env.fish
-
-# functions
-source $HOME/.config/fish/functions/zoxide.fish
--- private_dot_config/private_fish/private_functions/private_fish_prompt.fish
+++
@@ -1,7 +0,0 @@
-function fish_prompt
-    set_color $fish_color_cwd
-    echo -n (string replace -r "^$HOME" "~" $PWD)
-    set_color white
-    echo ':'
-    echo -n ' > '
-end
