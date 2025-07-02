b640a4a4e59878e12f7d9a9462fda66183fef9e4
Connor Rhodes <connor@rhodes.contact>
2025-07-02 11:59:11 -0500

    remove fish greeting

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
   source $HOME/.config/fish/aliases.fish
 end
 
+set -U fish_greeting ""
+
 function fish_prompt
   echo (set_color blue)(prompt_pwd)(set_color normal)
   echo -n '$ '
