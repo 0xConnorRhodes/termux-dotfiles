9d0c9c7fc3f32bd8183b7f549e778d8a7fa689c4
Connor Rhodes <connor@rhodes.contact>
2025-07-01 20:40:02 -0500

    update fish env

--- dot_config/fish/env.fish
+++ dot_config/fish/env.fish
@@ -1,4 +1,4 @@
 # paths
 #set --export PATH $HOME/.local/bin $PATH
-set --export NOTESDIR $HOME/storage/dcim/notes
+set --export NOTES $HOME/storage/dcim/notes
 set --export RUBYLIB "$HOME/code/ruby-modules/lib"
