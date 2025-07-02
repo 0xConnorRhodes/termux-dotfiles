3cbf8fdd4160a849f62408e9927832a9e7708ffb
Connor Rhodes <connor@rhodes.contact>
2025-07-01 20:00:55 -0500

    add fish shell

---
+++ dot_config/fish/alises.fish
@@ -0,0 +1,22 @@
+alias e="nvim"
+alias cm="chezmoi"
+alias gs="git status"
+alias fe='vim "$(fd -t f . | fzf)"'
+alias cmd="SHELL=/data/data/com.termux/files/usr/bin/fish chezmoi cd"
+alias cmu="chezmoi update --apply"
+alias gmm="git add . && git commit -m 'u' && git rebase --continue && git push"
+#alias ms="mosh s"
+#alias cat="bat --style=plain --pager=none"
+#alias sn="screen -c $HOME/.config/screen/screenrc"
+#alias py="python3"
+#alias rb="ruby"
+
+# notes aliases
+alias tk="ruby $HOME/code/notes-scripts/tasks-menu-phone.rb"
+alias mt="ruby $HOME/code/notes-scripts/new-meeting.rb"
+alias cb="ruby $HOME/code/food-log/add_entry.rb"
+
+# navigation
+alias x="exit"
+alias j="__zoxide_z"
+alias ji="__zoxide_zi"
---
+++ dot_config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    source $HOME/.config/fish/aliases.fish
+end
+
+source $HOME/.config/fish/env.fish
+
+# functions
+source $HOME/.config/fish/functions/zoxide.fish
