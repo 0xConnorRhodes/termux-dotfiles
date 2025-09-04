574b5ac8477c60d2ac5cbc28b3134ab19a96ea5c
Connor Rhodes <connor@rhodes.contact>
2025-09-03 20:21:12 -0500

    start nvim in insert mode

--- dot_config/private_nvim/private_init.lua
+++ dot_config/private_nvim/private_init.lua
@@ -1,3 +1,8 @@
 -- Map double <Esc> in normal mode to write and quit (like ZZ)
 vim.keymap.set("n", "<Esc>", "<cmd>x<CR>", { noremap = true, silent = true })
 
+-- Automatically start in insert mode on startup
+vim.api.nvim_create_autocmd('VimEnter', {
+  pattern = '*',
+  command = 'startinsert',
+})
